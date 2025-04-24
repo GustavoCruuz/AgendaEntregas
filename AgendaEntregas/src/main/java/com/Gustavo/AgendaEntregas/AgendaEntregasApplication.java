e37f5b6424123f3f87f47339e0b2e17eb803cff1
GustavoCruzDev <gustavocruz.profissional@gmail.com>
2025-04-24 15:29:08 -0300

    first commit

---
+++ AgendaEntregas/src/main/java/com/Gustavo/AgendaEntregas/AgendaEntregasApplication.java
@@ -0,0 +1,13 @@
+package com.Gustavo.AgendaEntregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaEntregasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaEntregasApplication.class, args);
+	}
+
+}
---
+++ AgendaEntregas/src/test/java/com/Gustavo/AgendaEntregas/AgendaEntregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gustavo.AgendaEntregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaEntregasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
